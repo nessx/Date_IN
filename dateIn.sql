597348c75fe55c2d731a2796953558865ffb23f2
nessx <cf17nestor.santana@iesjoandaustria.org>
2022-03-22 19:12:25 +0100

    reconstruccion db ahora funciona

--- dateIn.sql
+++ dateIn.sql
@@ -1,15 +1,57 @@
-CREATE DATABASE DateIn;
+-- --------------------------------------------------------
+-- Host:                         192.168.33.10
+-- Versión del servidor:         10.3.34-MariaDB-0ubuntu0.20.04.1 - Ubuntu 20.04
+-- SO del servidor:              debian-linux-gnu
+-- HeidiSQL Versión:             11.3.0.6295
+-- --------------------------------------------------------
 
-CREATE TABLE `DateIn`.`Users` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR NOT NULL , `last_name` VARCHAR NOT NULL , `email` VARCHAR NOT NULL , `password` VARCHAR NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `DateIn`.`Api-key` ( `id` VARCHAR(255) NOT NULL , `user_id` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
-ALTER TABLE `Api-key` CHANGE `user_id` `user_id` INT NOT NULL;
+-- Volcando estructura de base de datos para DateIn
+CREATE DATABASE IF NOT EXISTS `DateIn` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `DateIn`;
 
-ALTER TABLE `Api-key` ADD CONSTRAINT FK_USERS_APIKEY FOREIGN KEY (user_id) REFERENCES (id.Users);
+-- Volcando estructura para tabla DateIn.Actions
+CREATE TABLE IF NOT EXISTS `Actions` (
+  `id_user` int(11) NOT NULL,
+  `id_affected_user` int(11) NOT NULL,
+  `action` int(11) NOT NULL,
+  PRIMARY KEY (`id_user`,`id_affected_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `Users` ADD `description` VARCHAR(255) NOT NULL AFTER `password`;
+-- La exportación de datos fue deseleccionada.
 
-CREATE TABLE `DateIn`.`Actions` ( `id_user` INT NOT NULL , `id_affected_user` INT NOT NULL , `action` INT NOT NULL ) ENGINE = InnoDB;
+-- Volcando estructura para tabla DateIn.Api-key
+CREATE TABLE IF NOT EXISTS `Api-key` (
+  `id` varchar(255) NOT NULL DEFAULT '',
+  `user_id` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `FK_Api-key_Users` (`user_id`),
+  CONSTRAINT `FK_Api-key_Users` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `Actions` ADD PRIMARY KEY( `id_user`, `id_affected_user`);
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla DateIn.Users
+CREATE TABLE IF NOT EXISTS `Users` (
+  `id` varchar(50) NOT NULL DEFAULT 'AUTO_INCREMENT',
+  `name` varchar(255) NOT NULL DEFAULT '0',
+  `last_name` varchar(255) NOT NULL DEFAULT '0',
+  `email` varchar(255) NOT NULL DEFAULT '0',
+  `password` varchar(255) NOT NULL DEFAULT '0',
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- La exportación de datos fue deseleccionada.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
